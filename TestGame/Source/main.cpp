b1128594b4b5a298b4704049b043df8a6ef30389
Rasmus Tilljander <tilljander.rasmus@gmail.com>
2015-01-12 16:53:03 +0100

    Removed reference to the .zip archive used in a sideproject; also removed the include <fstream> as it was only used by the archiver

--- TestGame/Source/main.cpp
+++ TestGame/Source/main.cpp
@@ -2,31 +2,9 @@
 #if defined(WIN32) && !defined(UNIX)
 /* Do windows stuff */
 #include <Windows.h>
-#include <fstream>
-
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
 {
-	/*
-	ZipArchive::Ptr archive = ZipFile::Open("archive.zip");
-
-	ZipArchiveEntry::Ptr entry = archive->GetEntry("file.dat");
-
-	size_t t = entry->GetCompressedSize();
-	// if entry is nullptr, it means the file already exists in the archive
-	assert(entry != nullptr);
-
-	std::ifstream contentStream("file.dat", std::ios::binary);
-	
-	entry->SetCompressionStream(contentStream);
-
-	// you may of course save it somewhere else
-	// note: the contentStream is pumped when the archive is saved
-	// if you want to read the stream before saving of the archive,
-	// it is need to set Immediate mode in SetCompressionStream method (see below)
-	ZipFile::SaveAndClose(archive, "archive.zip");
-	*/
-
 	Jamgame* l_jamgame = new Jamgame();
 	l_jamgame->Initialize(hInstance, lpCmdLine, nCmdShow);
 	l_jamgame->Run();
